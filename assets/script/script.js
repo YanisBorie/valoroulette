6ce6b89fc03c5f7f9acd06024bc3ad69899f78e1
YanisBorie <yanis.borie69@gmail.com>
2023-07-30 00:47:45 +0200

    Initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+var duelist = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna"];
+
+var initiator = ["Breach","Fade","Kayo","Skye","Sova","Gekko"];
+
+var controler = ["Brimstone","Viper","Astra","Harbor","Omen"];
+
+var sentinel = ["Chamber","Cypher","Killjoy","Sage","Deadlock"];
