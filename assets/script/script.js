f0d6101ee6140c8cc5d3187ff3654ea5bd38c791
YanisBorie <yanis.borie69@gmail.com>
2023-07-30 04:12:44 +0200

    Design + toutes les fonctionnalités

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,55 @@
-var duelist = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna"];
+var allAgent = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna","Breach","Fade","Kayo","Skye","Sova","Gekko","Brimstone","Viper","Astra","Harbor","Omen","Chamber","Cypher","Killjoy","Sage","Deadlock"];
+var agentUnselected = [];
+var agentSelected = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna","Breach","Fade","Kayo","Skye","Sova","Gekko","Brimstone","Viper","Astra","Harbor","Omen","Chamber","Cypher","Killjoy","Sage","Deadlock"];
 
-var initiator = ["Breach","Fade","Kayo","Skye","Sova","Gekko"];
+function random() {
+    alert("Vous jouez " + agentSelected[Math.floor(Math.random() * agentSelected.length)]);
+}
 
-var controler = ["Brimstone","Viper","Astra","Harbor","Omen"];
+function select(agent) {
+    if (document.getElementById(agent).style.opacity == 0.25) {
+        document.getElementById(agent).style.opacity = 1;
+        agentSelected.push(agent);
+    } else {
+        document.getElementById(agent).style.opacity = 0.25;
+        agentSelected = agentSelected.filter((agentSelected) => agentSelected !== agent);
+    }
+}
 
-var sentinel = ["Chamber","Cypher","Killjoy","Sage","Deadlock"];
+function filterSelect(filter) {
+    switch(filter) {
+        case "All":
+            agentSelected = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna","Breach","Fade","Kayo","Skye","Sova","Gekko","Brimstone","Viper","Astra","Harbor","Omen","Chamber","Cypher","Killjoy","Sage","Deadlock"];
+            for(let i = 0; i <= agentSelected.length; i++) {
+                document.getElementById(agentSelected[i]).style.opacity = 1;
+            }
+            break;
+        case "Duelist":
+            agentSelected = ["Jett","Phoenix","Yoru","Raze","Neon","Reyna"];
+            filterUnselect();
+            break;
+        case "Initiator":
+            agentSelected = ["Breach","Fade","Kayo","Skye","Sova","Gekko"];
+            filterUnselect();
+            break;
+        case "Controler":
+            agentSelected = ["Brimstone","Viper","Astra","Harbor","Omen"];
+            filterUnselect();
+            break;
+        case "Sentinel":
+            agentSelected = ["Chamber","Cypher","Killjoy","Sage","Deadlock"];
+            filterUnselect();
+            break;
+    }
+}
+
+function filterUnselect() {
+    agentUnselected = allAgent;
+    for(let i = 0; i <= agentSelected.length; i++) {
+        agentUnselected = agentUnselected.filter((agentUnselected) => agentUnselected !== agentSelected[i]);
+    }
+
+    for(let i = 0; i <= agentUnselected.length; i++) {
+        document.getElementById(agentUnselected[i]).style.opacity = 0.25;
+    }
+}
